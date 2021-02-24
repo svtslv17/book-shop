5c8e91d4647f37e278976275e11d6323e1e95e64
svtslv17 <svtslv17@gmail.com>
2021-02-24 15:12:14 +0300

    refactor: apply eslint,prettier, stylelint, husky

--- src/app/components/book-component/book-component.component.ts
+++ src/app/components/book-component/book-component.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
-  selector: 'book-component',
-  templateUrl: './book-component.component.html',
-  styleUrls: ['./book-component.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-book-component',
+    templateUrl: './book-component.component.html',
+    styleUrls: ['./book-component.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BookComponent implements OnInit {
-  constructor() {}
+    constructor() {}
 
-  ngOnInit(): void {}
+    // ngOnInit(): void {
+
+    // }
 }
